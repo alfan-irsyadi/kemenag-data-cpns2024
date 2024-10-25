9888785a77b2009c63f4eabc8e4f4b742341c758
alfan-irsyadi <84897623+alfan-irsyadi@users.noreply.github.com>
2024-10-25 11:31:41 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -8,13 +8,18 @@ df = pd.read_csv('TABEL-JABATAN.csv')
 # st.write("Original Data:")
 # st.dataframe(df)
 
-# Input box to search by NAMA
-search_term = st.text_input("Search by NAMA:")
+search_nama = st.text_input("Search by NAMA:")
+search_jabatan = st.text_input("Search by JABATAN:")
 
-# If the user inputs a search term, filter the DataFrame
-if search_term:
-    filtered_df = df[df['NAMA'].str.contains(search_term, case=False, na=False)]
-    
-    # Display the filtered DataFrame
-    st.write("Search Results:")
-    st.dataframe(filtered_df)
+# If user inputs a search term, filter the DataFrame
+filtered_df = df.copy()  # Start with the original DataFrame
+
+if search_nama:
+    filtered_df = filtered_df[filtered_df['NAMA'].str.contains(search_nama, case=False, na=False)]
+
+if search_jabatan:
+    filtered_df = filtered_df[filtered_df['JABATAN'].str.contains(search_jabatan, case=False, na=False)]
+
+# Display the filtered DataFrame
+st.write("Search Results:")
+st.dataframe(filtered_df)
